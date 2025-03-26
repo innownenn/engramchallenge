88db0ece196dbb160d77196d511c53ddb59e02c9
Hanno van Baarle <hannovb@hotmail.be>
2025-03-26 21:35:41 +0100

    homepage added

---
+++ src/components/Homepage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="mainpage">
+    <h1>Home Page</h1>
+    <p>
+      This is the home page. You can edit the content of this page by editing.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="css" scoped></style>
