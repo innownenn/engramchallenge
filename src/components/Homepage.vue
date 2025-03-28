21517754a81294885eb8bd1d88f6b092887e2761
Hanno van Baarle <hannovb@hotmail.be>
2025-03-28 18:18:08 +0100

    Home page concept

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -4,6 +4,11 @@
     <p>
       This is the home page. You can edit the content of this page by editing.
     </p>
+    <div id="image-container">
+      <div v-for="(item, index) in 8" :key="index" class="image">
+        <h1>Cool image</h1>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,4 +20,28 @@ export default {
 };
 </script>
 
-<style lang="css" scoped></style>
+<style scoped>
+.mainpage {
+  width: 100%;
+  border: red solid 1px;
+}
+
+#image-container {
+  border: red solid 1px;
+  display: grid;
+  justify-content: center;
+  grid-template-columns: repeat(auto-fill, 200px);
+  gap: 20px;
+  padding: 10px;
+}
+
+.image {
+  width: 100%; /* Ensures flexibility */
+  max-width: 200px;
+  height: 200px;
+  background-color: #ccc;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  transition: transform 0.3s ease;
+}
+</style>
